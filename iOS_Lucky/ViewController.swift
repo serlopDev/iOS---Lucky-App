fbde081384783eeec59a473db2fb205236c51c97
Sergio López <sergio.lofer.dev@gmail.com>
2020-08-28 19:18:58 +0200

    Initial Commit

---
+++ iOS_Lucky/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_Lucky
+//
+//  Created by Sergio López on 28/08/2020.
+//  Copyright © 2020 Sergio López. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
